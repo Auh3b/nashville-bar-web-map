734f0b2dee91b13bba9e601ba81cac8ba7998fed
Auh3b <ngaiyayec@yahoo.com>
2025-03-01 01:50:43 +0200

    removed scroll effect

--- src/components/UI/BarsInfoUI.tsx
+++ src/components/UI/BarsInfoUI.tsx
@@ -4,27 +4,17 @@ import { LuBeerOff } from 'react-icons/lu';
 import useDataMap from '../../hooks/useDataMap';
 import useMapStore from '../../data/mapStore';
 import { IoCloseOutline } from 'react-icons/io5';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 
 export default function BarsInfoUI() {
   const ref = useRef<null | HTMLDivElement>(null);
   const bars = useDataMap('bars');
 
-  const { isDataLoaded, bar, preview, setExplore, isMobile } = useMapStore(
-    (state) => state,
-  );
+  const { isDataLoaded, setExplore, isMobile } = useMapStore((state) => state);
   const handleClose = () => {
     setExplore(false);
   };
 
-  // useEffect(() => {
-  //   if ((preview || bar) && isDataLoaded && ref.current) {
-  //     document
-  //       // @ts-ignore
-  //       .getElementById(preview?.bar ? preview.bar : bar.id)
-  //       ?.scrollIntoView({ behavior: 'smooth' });
-  //   }
-  // }, [bar, isDataLoaded, ref]);
   return (
     <div
       ref={ref}
