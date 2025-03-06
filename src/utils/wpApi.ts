e2e8b7c8c3a165d722ce5a30413f52b6615a89ef
Auh3b <ngaiyayec@yahoo.com>
2025-03-06 14:24:43 +0200

    change base url

--- src/utils/wpApi.ts
+++ src/utils/wpApi.ts
@@ -1,8 +1,15 @@
+const getBaseUrl = (): string => {
+  const regex = /^.*:(5173).*$/g;
+  const base = window.location.host;
+  const found = base.search(regex);
+  return found === -1 ? '' : 'http://localhost:8889';
+};
+
 export const getPosts = async (postType: string) => {
   try {
+    const baseUrl = getBaseUrl();
     const res = await fetch(
-      `
-      http://localhost:8889/index.php?rest_route=%2Fwp%2Fv2%2F${postType}&per_page=100&_locale=user`,
+      `${baseUrl}/index.php?rest_route=%2Fwp%2Fv2%2F${postType}&per_page=100&_locale=user`,
     );
     const data = await res.json();
     return data;
