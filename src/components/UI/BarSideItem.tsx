d840b56d60380d5c53ecf95d976bffbb307a56aa
Auh3b <ngaiyayec@yahoo.com>
2025-03-07 03:37:00 +0200

    change styling

--- src/components/UI/BarSideItem.tsx
+++ src/components/UI/BarSideItem.tsx
@@ -11,16 +11,16 @@ export default function BarSideItem(props: BarSideItemProps) {
         <div
           id={id}
           className={`flex flex-col`}>
-          <div className='flex items-center justify-between  p-3  cursor-pointer border-b border-primary'>
-            <span className='text-xs uppercase'>{name}</span>
+          <div className='flex items-center justify-between p-3 cursor-pointer border-b border-primary'>
+            <span className=' uppercase'>{name}</span>
           </div>
           <div
-            className={` transition-all duration-500 ease-in-out  scrollbar-none max-h-fit`}>
+            className={`transition-all duration-500 ease-in-out  scrollbar-none max-h-fit`}>
             <div className='px-4 py-4 max-h-1/2'>
               <ElfsightWidget widgetId={igWidgetId} />
             </div>
             <div className='mb-4 px-4  text-sm'>📌{address}</div>
-            <div className='px-4 mb-4 text-xs'>{description}</div>
+            <div className='px-4 mb-4'>{description}</div>
           </div>
         </div>
       )}
